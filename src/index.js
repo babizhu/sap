1c492805a9f8aada3442443a03e2741e97916c81
liu_kun <liu_kun@msn.com>
2015-12-23 17:12:45 +0800

    redux的一个例子

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from 'react-dom';
+//import { App } from './App';
+
+//import 'babel-core/polyfill'
+import { Provider } from 'react-redux'
+import App from './experiment/async/containers/App'
+import configureStore from './experiment/async/store/configureStore'
+
+const store = configureStore()
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
+
