9ae852efdbeb1c417337fec1ec0d1348ea33eefb
liu_kun <liu_kun@msn.com>
2015-12-25 18:07:02 +0800

    继续学习函数式编程

--- src/experiment/async/actions/index.js
+++ src/experiment/async/actions/index.js
@@ -6,64 +6,65 @@ export const SELECT_REDDIT = 'SELECT_REDDIT';
 export const INVALIDATE_REDDIT = 'INVALIDATE_REDDIT';
 
 export function selectReddit(reddit) {
-  return {
-    type: SELECT_REDDIT,
-    reddit
-  }
+    return {
+        type: SELECT_REDDIT,
+        reddit
+    }
 }
 
 export function invalidateReddit(reddit) {
-  return {
-    type: INVALIDATE_REDDIT,
-    reddit
-  }
+    return {
+        type: INVALIDATE_REDDIT,
+        reddit
+    }
 }
 
 function requestPosts(reddit) {
-  return {
-    type: REQUEST_POSTS,
-    reddit
-  }
+    return {
+        type: REQUEST_POSTS,
+        reddit
+    }
 }
 
 function receivePosts(reddit, json) {
     let map = json.data.children.map(child => child.data);
     //console.log("json=" + JSON.stringify(json));
-    for (let value of map) {
-        console.log(value);
+    //for (let value of map) {
+    //    console.log(value);
+    //}
+    return {
+        type: RECEIVE_POSTS,
+        reddit: reddit,
+        posts: json.data.children.map(child => child.data),
+        receivedAt: Date.now()
     }
-  return {
-    type: RECEIVE_POSTS,
-    reddit: reddit,
-    posts: json.data.children.map(child => child.data),
-    receivedAt: Date.now()
-  }
 }
 
 function fetchPosts(reddit) {
-  return dispatch => {
-    dispatch(requestPosts(reddit));
-    return fetch(`http://www.reddit.com/r/${reddit}.json`)
-      .then(response => response.json())
-      .then(json => dispatch(receivePosts(reddit, json)))
-  }
+    return dispatch => {
+        dispatch(requestPosts(reddit));
+
+        return fetch(`http://www.reddit.com/r/${reddit}.json`)
+            .then(response => response.json())
+            .then(json => dispatch(receivePosts(reddit, json)))
+    }
 }
 
 function shouldFetchPosts(state, reddit) {
-  const posts = state.postsByReddit[reddit]
-  if (!posts) {
-    return true
-  }
-  if (posts.isFetching) {
-    return false
-  }
-  return posts.didInvalidate
+    const posts = state.postsByReddit[reddit]
+    if (!posts) {
+        return true
+    }
+    if (posts.isFetching) {
+        return false
+    }
+    return posts.didInvalidate
 }
 
 export function fetchPostsIfNeeded(reddit) {
-  return (dispatch, getState) => {
-    if (shouldFetchPosts(getState(), reddit)) {
-      return dispatch(fetchPosts(reddit))
+    return (dispatch, getState) => {
+        if (shouldFetchPosts(getState(), reddit)) {
+            return dispatch(fetchPosts(reddit))
+        }
     }
-  }
 }
--- src/experiment/async/containers/App.js
+++ src/experiment/async/containers/App.js
@@ -32,60 +32,122 @@ class App extends Component {
     }
 
     printState(){
-        function multiply(x){
-            let result = 2*x;
+        //function multiply(x){
+        //    let result = 2*x;
+        //
+        //    return function(y){
+        //        let xy= result*y;
+        //        return function(z){
+        //            return xy*z;
+        //        }
+        //
+        //
+        //    }
+        //}
+        //const method=(x,s)=>y=>z=>{
+        //    return x*y*z*s;
+        //}
+        //
+        //
+        ////console.log("multiply(40)(10)=" + multiply(40)(10));
+        ////console.log("method(40)(10)(2)=" + method(40)(10)(2));
+        ////console.log("method(40)(10)=" + method(40)(10));
+        ////console.log("method(40)(10)(11)=" + method(40)(10)(11));
+        ////console.log("method(40,2)(10)(11)=" + method(40,2)(10)(11));
+        //
+        //const m1 = msg=>func=>{
+        //    func(msg);
+        //}
+        //
+        //const func1=msg=>{
+        //    console.log(msg)
+        //}
+        // m1(2567890)(func1);
+        //
+        //function applyMiddleware( ...middlewares ) {
+        //    return (next) =>
+        //        (reducer, initialState)=> {
+        //            let store = next(reducer, initialState);
+        //            let dispatch = store.dispatch;
+        //            return {
+        //                store,
+        //                dispatch
+        //            }
+        //        }
+        //
+        //}
+        //
+        //const func2 = function( reducer,initialState){
+        //    return {
+        //        store: reducer+initialState,
+        //        dispatch :'dispatch'
+        //    }
+        //}
+        //console.log('applyMiddleware( 23 )(func2)( \'aaaa\',\'bbb\')=' + applyMiddleware( 23 )(func2)( 'aaaa','bbb'));
 
-            return function(y){
-                let xy= result*y;
-                return function(z){
-                    return xy*z;
-                }
+        const mutpt = function(x,y){
+            return 2*x*y;
+        };
+        const add = function(x,y){
+            return 2+x+y;
+        };
+        const func = function( number, method ){
+            return method(number);
+        }
 
+        console.log( func(3,add));
 
+        function fetchPostsIfNeeded(reddit) {
+            return (dispatch, getState) => {
+                console.log( dispatch + ' ' + getState);
             }
         }
-        const method=(x,s)=>y=>z=>{
-            return x*y*z*s;
+
+
+        //console.log( 'fetchPostsIfNeeded(\'a\') = ' + fetchPostsIfNeeded('a'));
+
+        const test = function (index,name) {
+            console.log('arguments=' + JSON.stringify(arguments) );
+            console.log(  [].slice.call(arguments, 0) );
+            console.log(  [].slice.call(arguments, 1) );
+            console.log(  [].slice.call(arguments, 2) );
+        };
+
+        test( 333, 'liulaoye');
+
+        const test2=function(){
+            console.log('abcdef');
         }
+        const test1 = function( fn ){
+            fn.apply( );
+        };
+        test1( test2 );
 
-
-        //console.log("multiply(40)(10)=" + multiply(40)(10));
-        //console.log("method(40)(10)(2)=" + method(40)(10)(2));
-        //console.log("method(40)(10)=" + method(40)(10));
-        //console.log("method(40)(10)(11)=" + method(40)(10)(11));
-        //console.log("method(40,2)(10)(11)=" + method(40,2)(10)(11));
-
-        const m1 = msg=>func=>{
-            func(msg);
-        }
-
-        const func1=msg=>{
-            console.log(msg)
-        }
-         m1(2567890)(func1);
-
-        function applyMiddleware( ...middlewares ) {
-            return (next) =>
-                (reducer, initialState)=> {
-                    let store = next(reducer, initialState);
-                    let dispatch = store.dispatch;
-                    return {
-                        store,
-                        dispatch
-                    }
-                }
-
-        }
-
-        const func2 = function( reducer,initialState){
-            return {
-                store: reducer+initialState,
-                dispatch :'dispatch'
-            }
-        }
-        console.log('applyMiddleware( 23 )(func2)( \'aaaa\',\'bbb\')=' + applyMiddleware( 23 )(func2)( 'aaaa','bbb'));
-
-
+        //var currying = function(fn) {
+        //    // fn 指官员消化老婆的手段
+        //    console.log('arguments=' + JSON.stringify(arguments) );
+        //    var args = [].slice.call(arguments, 1);
+        //    console.log('[].slice.call(arguments, 1)=' + args );
+        //    // args 指的是那个合法老婆
+        //    return function() {
+        //        // 已经有的老婆和新搞定的老婆们合成一体，方便控制
+        //        var newArgs = args.concat([].slice.call(arguments));
+        //        // 这些老婆们用 fn 这个手段消化利用，完成韦小宝前辈的壮举并返回
+        //        return fn.apply(null, newArgs);
+        //    };
+        //};
+        //
+        //var getWife = currying(function() {
+        //    var allWife = [].slice.call(arguments);
+        //    // allwife 就是所有的老婆的，包括暗渡陈仓进来的老婆
+        //    console.log(allWife.join(";"));
+        //}, "合法老婆");
+        //
+        //// 获得其他6个老婆
+        //getWife("大老婆","小老婆","俏老婆","刁蛮老婆","乖老婆","送上门老婆");
+        //
+        //// 换一批老婆
+        //getWife("超越韦小宝的老婆");
 
     }
 
@@ -153,7 +215,7 @@ function mapStateToProps(state) {
         } = postsByReddit[selectedReddit] || {
         isFetching: true,
         items: []
-    }
+    };
 
     return {
         selectedReddit,
